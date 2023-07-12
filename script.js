4a62b1f3ef7b6276c2600f2d5b07cf6ec3014717
Darksied95 <aliyuefe9501@gmail.com>
2023-07-12 17:00:20 +0100

    init pen

---
+++ script.js
@@ -0,0 +1,13 @@
+const canvas = document.querySelector('canvas')
+const pen = canvas.getContext('2d')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+pen.fillRect(20, 20, 100, 100)
+pen.fillRect(440, 20, 100, 100)
+
+
+
+
+
